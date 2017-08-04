b33cbb7a016634ef9cad69b47ecc25312b7af590
Thomas Gros <thomas.gros@aiconoa.com>
2017-08-04 09:58:08 +0200

    repository initialization

---
+++ src/main/java/com/aiconoa/trainings/reactivewebsocketmicroservicewithspring5/ReactiveWebsocketsMicroservicesWithSpring5Application.java
@@ -0,0 +1,12 @@
+package com.aiconoa.trainings.reactivewebsocketmicroservicewithspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebsocketsMicroservicesWithSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebsocketsMicroservicesWithSpring5Application.class, args);
+	}
+}
